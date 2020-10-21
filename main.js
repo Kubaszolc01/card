cd8aecb6b08c1714e332a51f086f60f601d15275
Kubaszolc01 <42960682+Kubaszolc01@users.noreply.github.com>
2020-10-21 09:24:11 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
 const cart = document.querySelector(".cart");
-const buy = document.querySelector(".buy");
+const buy = document.querySelector(".buyBefore");
 const black = document.querySelector(".black");
+const colors = document.querySelector(".colors");
+const selectColor = document.querySelector(".selectColorBefore");
+const colorRed = document.querySelector(".colorRed");
+const colorYel = document.querySelector(".colorYel");
+const colorGre = document.querySelector(".colorGre");
+const image = document.querySelector(".image");
 
 console.log(cart);
 
 cart.addEventListener("click", function(){
-    buy.style.display = "block";
+    buy.className = "buyAfter"
+    black.style.opacity = "0.5";
+})
+colors.addEventListener("click", function(){
+    selectColor.className = "selectColorAfter"
     black.style.opacity = "0.5";
 })
 
 buy.addEventListener("mouseleave", function(){
-    buy.style.display = "none";
     black.style.opacity = "0";
+    buy.className = "buyBefore";
+})
+selectColor.addEventListener("mouseleave", function(){
+    black.style.opacity = "0";
+    selectColor.className = "selectColorBefore";
+})
+
+colorYel.addEventListener("click", function(){
+    image.src = "chair-yellow.jpg"
+})
+colorRed.addEventListener("click", function(){
+    image.src = "chair.jpg"
+})
+colorGre.addEventListener("click", function(){
+    image.src = "chair-green.jpg"
 })
